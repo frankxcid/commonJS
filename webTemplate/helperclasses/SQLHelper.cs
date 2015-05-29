eb2c3ff2e0eba6cc234cd6ab69b121a9e981e523
Frank Cedeno <frank.cedeno@nelsonstud.com>
2015-05-29 11:00:08 -0400

    Remove Reference to obsolete IntranetToolsASP4_5

--- webTemplate/helperclasses/SQLHelper.cs
+++ webTemplate/helperclasses/SQLHelper.cs
@@ -2,10 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using intranetTools4_5;
 using SQLConnectLibrary;
 
-namespace WorkOrderV2
+namespace webTemplate
 {
     public class SQLHelper
     {
